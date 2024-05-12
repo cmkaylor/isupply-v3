275129bdf95a07a956e8f30dc6e77c3bb475ac82
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-05-12 00:08:02 -0500

    Fix footer and clean css

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import AboutCarousel from "../components/AboutCarousel";
 
 const About = () => {
   return (
-    <Flex justify="top" align="center" flex="auto" vertical={true}>
+    <Flex justify="top" align="center" flex="auto" vertical={true} style={{height: '70vh'}}>
         <h1>About Us</h1>
         <div style={{width: '70vh'}}>
             <AboutCarousel/>
