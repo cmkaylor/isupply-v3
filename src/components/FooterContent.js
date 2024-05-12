3cb87c8ac531fb15770e9b77095fbceae9969c35
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-05-12 00:14:26 -0500

    Fix footer

--- src/components/FooterContent.js
+++ src/components/FooterContent.js
@@ -6,7 +6,7 @@ const FooterContent = () => {
 
     return (
         <footer>
-            <Flex justify="center" align="center" flex="auto">
+            <Flex justify="center" align="center" flex="auto" style={{textAlign: 'center'}}>
                 <p>&copy; {new Date().getFullYear()} Industrial Supply & Service, LLC. All rights reserved.</p>
             </Flex>
         </footer>
