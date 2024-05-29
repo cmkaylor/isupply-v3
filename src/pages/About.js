3c15e7c1552e9a4c487ff420666e5dce7b5b41ee
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-05-29 00:22:37 -0500

    Add new about page (from scratch)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,22 @@
 import { Flex } from "antd";
-import AboutCarousel from "../components/AboutCarousel";
 
 const About = () => {
   return (
-    <Flex justify="top" align="center" flex="auto" vertical={true} style={{height: '70vh'}}>
+    <Flex justify="top" align="center" flex="auto" vertical={true} style={{ backgroundColor: '#34495E', color: '#FFFFFF', height: '70vh'}}>
         <h2>About Us</h2>
-        <AboutCarousel/>
+        <p>
+        Located in Kansas City, Missouri since 2015, we offer a comprehensive range of top-quality fasteners, cutting tools,
+        shop supplies, PPE, and more. We serve many industries including; construction, automotive repair, fleet management, 
+        energy production, municipal services, and more.
+        </p>
+        <p>
+        At Industrial Supply & Service, we believe in the power of relationships. With years of experience in the industrial supply industry,
+        we take great pride in being a trusted partner with our customers, distributors, and manufacturers. Our mission is to provide our customers
+        with exceptional products and outstanding service. Our goal is to be your number one source for all your industrial MRO supply and service requirements. 
+        </p>
+        <p>
+        We are the exclusive representative of GARD Specialists Company and the Gardsert Thread Repair System for the Central States Region. 
+        </p>
     </Flex>
   )
 };
