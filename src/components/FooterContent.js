7c1f126b248d6436311ea372383b4a2f9aa7ca8f
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-05-28 23:18:15 -0500

    set footer to bottom

--- src/components/FooterContent.js
+++ src/components/FooterContent.js
@@ -5,10 +5,8 @@ import { Flex } from "antd";
 const FooterContent = () => {
 
     return (
-        <footer>
-            <Flex justify="center" align="center" flex="auto" style={{textAlign: 'center'}}>
-                <p>&copy; {new Date().getFullYear()} Industrial Supply & Service, LLC. All rights reserved.</p>
-            </Flex>
+        <footer style={{  bottom: '0', textAlign: 'center' }}> 
+            <p>&copy; {new Date().getFullYear()} Industrial Supply & Service, LLC. All rights reserved.</p>
         </footer>
     )
 }
