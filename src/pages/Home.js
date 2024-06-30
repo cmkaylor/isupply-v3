171eb7216ffbd9411a5abbf69d22a6951001fc47
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2024-06-30 01:05:34 -0400

    Make home page less tacky

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Flex, Button} from "antd";
+import { Typography, Flex, Button, Row, Col} from "antd";
 import Screws from '../content/screws.jpg'
 import ContactModal from "../components/ContactModal";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
   const [isContactOpen, setIsContactOpen] = useState(false);
 
   const showContact = () => {
@@ -14,19 +16,33 @@ const Home = () => {
     setIsContactOpen(false);
   }
 
+  const { Title } = Typography;
+
   return (
-    <Flex justify="center" align="center" flex="auto" style={{backgroundImage:`url(${Screws})`, backgroundPosition: 'center', backgroundSize: 'cover', height: '70vh'}}>
-        <div style={{backgroundColor:'#FFFFFF', width: '80vh', height: '35vh', margin: '20px', borderRadius: '25px', border: '4px solid #34495E', padding:'10px'}}>
-            <Flex justify="center" align="center" vertical={true}>
-                <h2 style={{color: '#34495E', fontWeight: 'bold', marginBottom: '0'}}>Welcome</h2>
-                <p align="center">Please click below and let us help you succeed in your industrial MRO supplying needs!</p>
-                <Flex justify="space-evenly" align="center">
-                  <Button onClick={showContact} style={{marginTop: '20px', backgroundColor: '#34495E', color: '#FFFFFF', width: '150px', height: '75px'}}>Contact Us</Button>
-                  <ContactModal open={isContactOpen} handleCancel={handleCancelContact}/>
+    <div style={{backgroundImage:`url(${Screws})`, backgroundPosition: 'center', backgroundSize: 'cover', height: '70vh'}}>
+      <Row>
+        <Col className="add-gradient" xs={24} xl={18}>
+          <Row>
+            <Col xs={24} xl ={16}>
+            <Flex justify="center" align="center" vertical={true} style={{height: '70vh'}}>
+                <Title style={{color: '#34495E'}}>Source, Supply, and Manage.</Title>
+                <Title level={5} style={{color: '#34495E', textAlign: 'center', margin: '10px'}}>Industrial Supply & Service is your new one stop shop for a variety of industrial solutions and products.</Title>
+                <Flex>
+                  <Flex justify="space-evenly" align="center" style={{margin:'8px'}}>
+                    <Button onClick={() => navigate('/Services')} style={{marginTop: '20px', backgroundColor: '#34495E', color: '#FFFFFF', width: '150px', height: '75px'}}><strong>Services</strong></Button>
+                  </Flex>
+                  <Flex justify="space-evenly" align="center" style={{margin:'8px'}}>
+                    <Button onClick={showContact} style={{marginTop: '20px', borderColor: '#34495E', color: '#34495E', width: '150px', height: '75px'}}><strong>Contact Us</strong></Button>
+                    <ContactModal open={isContactOpen} handleCancel={handleCancelContact}/>
+                  </Flex>
                 </Flex>
             </Flex>
-        </div>
-    </Flex>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} xl={12}/>
+      </Row>
+    </div>
   )
 };
 export default Home;
